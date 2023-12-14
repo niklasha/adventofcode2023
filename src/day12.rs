25b91e681e1410607ae6598e2ff50258e625a401
Niklas Hallqvist <niklas+github@appli.se>
2023-12-14 09:32:19 +0100

    wip

--- src/day12.rs
+++ src/day12.rs
@@ -33,16 +33,24 @@ impl Day12 {
         if groups.len() != vec.len() {
             return false;
         }
-        groups
+        let rv = groups
             .into_iter()
             .zip(vec.iter())
-            .all(|(springs, &len)| springs.len() == len)
+            .all(|(springs, &len)| springs.len() == len);
+        rv
     }
 
     fn unfold(springs: &str, ranges: &str) -> (String, String) {
         (
             iter::repeat(springs).take(5).join("?"),
             iter::repeat(ranges).take(5).join(","),
+        )
+    }
+
+    fn unfold_2(springs: &str, ranges: &str, n: usize) -> (String, String) {
+        (
+            iter::repeat(springs).take(n).join("."),
+            iter::repeat(ranges).take(n).join(","),
         )
     }
 
@@ -130,8 +138,38 @@ impl Day12 {
             .iter()
             .filter(|(_, v)| v.is_empty() || v.len() == 1 && v[0] == 0)
             .count();
-//        println!("{}", rv);
+        //    println!("{}", rv);
         rv
+    }
+
+    fn validate_3(springs: &[u8], ranges: &Vec<Output>) -> Output {
+        let springs = String::from_utf8(springs.to_owned()).unwrap();
+        let v = (1..=5)
+            .map(|i| {
+                let (springs, ranges) = (
+                    iter::repeat(&springs)
+                        .take(i)
+                        .join("#")
+                        .as_bytes()
+                        .to_owned(),
+                    ranges
+                        .iter()
+                        .copied()
+                        .cycle()
+                        .take(i * ranges.len())
+                        .collect_vec(),
+                );
+                println!("{:?} {:?}", springs, ranges);
+                Self::validate_2(&springs, &ranges)
+            })
+            .collect_vec();
+        v[0].pow(5)
+            + 4 * v[0].pow(3) * v[1]
+            + 3 * v[0].pow(2) * v[2]
+            + 3 * v[0] * v[1].pow(2)
+            + 2 * v[0] * v[3]
+            + 2 * v[1] * v[2]
+            + v[4]
     }
 
     fn process_1(input: &mut dyn io::Read) -> BoxResult<Output> {
@@ -165,8 +203,29 @@ impl Day12 {
             })
             .collect::<Result<Vec<_>, AocError>>()?
             .into_iter()
-            //            .map(|c| { println!("{}", c); c })
+            .map(|c| {
+                println!("{}", c);
+                c
+            })
             .sum())
+    }
+
+    // Disperse n resources over m slots
+    fn disperse(mut p: Vec<usize>, n: usize, m: usize) -> Vec<Vec<usize>> {
+        if p.len() == m {
+            vec![p]
+        } else if p.len() == m - 1 {
+            p.push(n);
+            vec![p]
+        } else {
+            (0..=n)
+                .flat_map(|i| {
+                    let mut p = p.clone();
+                    p.push(i);
+                    Self::disperse(p, n - i, m).into_iter()
+                })
+                .collect_vec()
+        }
     }
 
     fn process_2(input: &mut dyn io::Read, is_folded: bool) -> BoxResult<Output> {
@@ -190,11 +249,139 @@ impl Day12 {
                         .map(|s| Ok(s.parse()?))
                         .collect::<BoxResult<Vec<Output>>>()
                         .map_err(|_| AocError)?;
-                    Ok(Self::validate_2(springs, &ranges))
+                    Ok(if is_folded {
+                        println!("{:?} {:?}", springs, ranges);
+                        let mut dispersions = Self::disperse(
+                            vec![],
+                            springs.len() - (ranges.iter().sum::<Output>() + ranges.len() - 1),
+                            ranges.len() + 1,
+                        );
+                        println!("{}", dispersions.len());
+                        for mut dispersion in &mut dispersions {
+                            for i in 1..(dispersion.len() - 1) {
+                                dispersion[i] += 1;
+                            }
+                        }
+                        let candidates = dispersions
+                            .iter()
+                            .map(|dispersion| {
+                                let mut s = ranges.iter().enumerate().fold(
+                                    String::new(),
+                                    |mut s, (i, &range)| {
+                                        s.push_str(&String::from(".").repeat(dispersion[i]));
+                                        s.push_str(&String::from("#").repeat(range));
+                                        s
+                                    },
+                                );
+                                s.push_str(&String::from(".").repeat(*dispersion.last().unwrap()));
+                                s
+                            })
+                            .collect_vec();
+                        let mut s = String::from_utf8(springs.to_owned()).unwrap();
+                        s = s.replace('.', "\\.");
+                        s = s.replace('?', ".");
+                        //                        println!("{}", s);
+                        let re = Regex::new(&s).unwrap();
+                        let x = candidates
+                            .into_iter()
+                            .filter(|s| re.is_match(s))
+                            .collect_vec();
+                        println!("{}", x.len());
+                        x.len()
+                    //                        println!("{:?}", x);
+                    //                        let rv = Self::validate_3(springs, &ranges);
+                    //                        println!("{}", rv);
+                    //                        rv
+                    } else {
+                        Self::validate_2(springs, &ranges)
+                    })
                 })
             })
             .collect::<Result<Vec<_>, AocError>>()?
             .into_iter()
+            // .map(|c| {
+            //     println!("{}", c);
+            //     c
+            // })
+            .sum())
+    }
+
+    fn process_3(input: &mut dyn io::Read, is_folded: bool) -> BoxResult<Output> {
+        Ok(io::BufReader::new(input)
+            .lines()
+            .map(|rs| {
+                rs.map_err(|_| AocError).and_then(|s| {
+                    let mut tokens = s.split_whitespace();
+                    let (springs, ranges) = (
+                        tokens.next().ok_or(AocError)?,
+                        tokens.next().ok_or(AocError)?,
+                    );
+                    let (mut springs, mut ranges) = (springs.to_string(), ranges.to_string());
+                    println!("#1: {:?} {:?}", springs, ranges);
+                    if is_folded {
+                        (springs, ranges) = Self::unfold_2(&springs, &ranges, 5);
+                        println!("#2: {:?} {:?}", springs, ranges);
+                    }
+                    let unknowns = springs.match_indices('?').map(|(i, _)| i).collect_vec();
+                    let springs = springs.as_bytes();
+                    let ranges = ranges
+                        .split(',')
+                        .map(|s| Ok(s.parse()?))
+                        .collect::<BoxResult<Vec<Output>>>()
+                        .map_err(|_| AocError)?;
+                    Ok(if is_folded {
+                        let mut dispersions = Self::disperse(
+                            vec![],
+                            springs.len() - (ranges.iter().sum::<Output>() + ranges.len() - 1),
+                            ranges.len() + 1,
+                        );
+                        println!("{}", dispersions.len());
+                        for mut dispersion in &mut dispersions {
+                            for i in 1..(dispersion.len() - 1) {
+                                dispersion[i] += 1;
+                            }
+                        }
+                        let candidates = dispersions
+                            .iter()
+                            .map(|dispersion| {
+                                let mut s = ranges.iter().enumerate().fold(
+                                    String::new(),
+                                    |mut s, (i, &range)| {
+                                        s.push_str(&String::from(".").repeat(dispersion[i]));
+                                        s.push_str(&String::from("#").repeat(range));
+                                        s
+                                    },
+                                );
+                                s.push_str(&String::from(".").repeat(*dispersion.last().unwrap()));
+                                s
+                            })
+                            .collect_vec();
+                        let mut s = String::from_utf8(springs.to_owned()).unwrap();
+                        s = s.replace('.', "\\.");
+                        s = s.replace('?', ".");
+                        //                        println!("{}", s);
+                        let re = Regex::new(&s).unwrap();
+                        let x = candidates
+                            .into_iter()
+                            .filter(|s| re.is_match(s))
+                            .collect_vec();
+                        println!("{}", x.len());
+                        x.len()
+                    //                        println!("{:?}", x);
+                    //                        let rv = Self::validate_3(springs, &ranges);
+                    //                        println!("{}", rv);
+                    //                        rv
+                    } else {
+                        Self::validate_2(springs, &ranges)
+                    })
+                })
+            })
+            .collect::<Result<Vec<_>, AocError>>()?
+            .into_iter()
+            // .map(|c| {
+            //     println!("{}", c);
+            //     c
+            // })
             .sum())
     }
 
@@ -203,7 +390,7 @@ impl Day12 {
     }
 
     fn part2_impl(&self, input: &mut dyn io::Read) -> BoxResult<Output> {
-        Self::process_2(input, true)
+        Self::process_3(input, true)
     }
 }
 
@@ -215,28 +402,27 @@ mod tests {
         assert_eq!(Day12 {}.part1_impl(&mut s.as_bytes()).ok(), Some(f));
     }
 
-        #[test]
-        fn part1() {
-            test1(
-                "#.#.### 1,1,3
+    #[test]
+    fn part1() {
+        test1(
+            "#.#.### 1,1,3
     .#...#....###. 1,1,3
     .#.###.#.###### 1,3,1,6
     ####.#...#... 4,1,1
     #....######..#####. 1,6,5
     .###.##....# 3,2,1",
-                6,
-            );
-            test1(
-                "???.### 1,1,3
+            6,
+        );
+        test1(
+            "???.### 1,1,3
     .??..??...?##. 1,1,3
     ?#?#?#?#?#?#?#? 1,3,1,6
     ????.#...#... 4,1,1
     ????.######..#####. 1,6,5
-    ?###???????? 3,2,1
-    ",
-                21,
-            );
-        }
+    ?###???????? 3,2,1",
+            21,
+        );
+    }
 
     fn test2(s: &str, f: Output) {
         assert_eq!(Day12 {}.part2_impl(&mut s.as_bytes()).ok(), Some(f));
